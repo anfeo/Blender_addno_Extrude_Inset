55708800c2d57af305209a56bc41866819bea676
Alfonso Annarumma <a.annarumma@primalshapestudio.com>
2023-12-08 11:47:38 +0100

    Update error

--- __init__.py
+++ __init__.py
@@ -8,14 +8,15 @@ bl_info = {
     "warning": "",
     "wiki_url": "",
     "category": "Mesh",
-    }
-
+}
 
 import bpy
 import bmesh
 from bpy.props import IntProperty, FloatProperty
 from mathutils import Vector
-def main(context,thickness,extrude,loop,thick_loop):
+
+
+def main(context, thickness, extrude, loop, thick_loop):
     obj = bpy.context.edit_object
     me = obj.data
     faces = []
@@ -26,18 +27,18 @@ def main(context,thickness,extrude,loop,thick_loop):
     for f in bm.faces:
         if f.select:
             faces.append(f)
-            
-    #face = bm.faces.active
-        
+
+    # face = bm.faces.active
 
     if thickness > 0.0:
-        for tl in range(0,thick_loop+1):
-            bmesh.ops.inset_region(bm, faces = faces, thickness=(thickness/(thick_loop+1)), depth=0, use_even_offset=True)
+        for tl in range(0, thick_loop + 1):
+            bmesh.ops.inset_region(bm, faces=faces, thickness=(thickness / (thick_loop + 1)), depth=0,
+                                   use_even_offset=True)
     if abs(extrude) > 0.0:
-        
-        #verts.append(v for f in face for v in f.verts)
-        for l in range(0,loop+1):
-            bmesh.ops.inset_region(bm, faces = faces, thickness=0, depth=0, use_even_offset=True)
+
+        # verts.append(v for f in face for v in f.verts)
+        for l in range(0, loop + 1):
+            bmesh.ops.inset_region(bm, faces=faces, thickness=0, depth=0, use_even_offset=True)
             for f in faces:
                 if f.select:
                     normals.append(f.normal)
@@ -45,10 +46,10 @@ def main(context,thickness,extrude,loop,thick_loop):
                         if v not in verts:
                             verts.append(v)
             normal = sum(normals, Vector()) / len(normals)
-            
-            bmesh.ops.translate(bm, verts = verts, vec = normal * (extrude/(loop+1)))
+
+            bmesh.ops.translate(bm, verts=verts, vec=normal * (extrude / (loop + 1)))
             verts = []
-            #print(geom)
+            # print(geom)
     # Show the updates in the viewport
     # and recalculate n-gon tessellation.
     bmesh.update_edit_mesh(me, loop_triangles=True, destructive=True)
@@ -59,37 +60,36 @@ class Mesh_OT_Extrude_Inset(bpy.types.Operator):
     bl_idname = "mesh.extrude_inset"
     bl_label = "Extrude and Inset"
     bl_options = {'REGISTER', 'UNDO'}
-    
-    
-    thickness : FloatProperty(name="Inset", default=0.0)
-    thick_loop : IntProperty(name="Inset Loops", default=0, min=0)
-    extrude : FloatProperty(name="Extrude", default=0.0)
-    loop : IntProperty(name="Extrude Loops", default=0, min=0)
-    
-    
+
+    thickness: FloatProperty(name="Inset", default=0.0)
+    thick_loop: IntProperty(name="Inset Loops", default=0, min=0)
+    extrude: FloatProperty(name="Extrude", default=0.0)
+    loop: IntProperty(name="Extrude Loops", default=0, min=0)
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
 
     def execute(self, context):
-        #print("esegui")
-        main(context,self.thickness,self.extrude,self.loop,self.thick_loop)
+        # print("esegui")
+        main(context, self.thickness, self.extrude, self.loop, self.thick_loop)
         return {'FINISHED'}
 
+
 def menu_func(self, context):
-    
     layout = self.layout
     layout.operator("mesh.extrude_inset")
+
 
 def register():
     bpy.utils.register_class(Mesh_OT_Extrude_Inset)
     bpy.types.VIEW3D_MT_edit_mesh_faces.append(menu_func)
 
+
 def unregister():
     bpy.utils.unregister_class(Mesh_OT_Extrude_Inset)
     bpy.types.VIEW3D_MT_edit_mesh_faces.remove(menu_func)
 
+
 if __name__ == "__main__":
     register()
-
-   
